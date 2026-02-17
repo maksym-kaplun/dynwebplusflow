2f29b4aa96c2cf1004de0bd293ed3f5dd3281c7f
maksym-kaplun <kaplun.maksym@zak.spspb.cz>
2026-02-17 11:28:17 +0000

    feat(js): render name and skills list

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 
-fetch('./profile.json') 
+const API_URL = './profile.json';
+
+fetch(API_URL)
     .then(response => {
-        return response.json(); 
+        if (!response.ok) {
+            throw new Error('Nepodařilo se načíst data');
+        }
+        return response.json();
     })
     .then(data => {
-        console.log("Úspěch! Data jsou tady:", data); 
+        document.getElementById('name').textContent = data.name;
+
+        const skillsList = document.getElementById('skills-list');
+
+        skillsList.innerHTML = '';
+
+        data.skills.forEach(skill => {
+            const li = document.createElement('li'); 
+            li.textContent = skill;                  
+            skillsList.appendChild(li);              
+        });
+
+        const interestsSection = document.getElementById('interests-section');
+        
+        interestsSection.innerHTML = '';
+
+        data.interests.forEach(interest => {
+            const div = document.createElement('div');
+            div.classList.add('interest-item'); 
+            div.textContent = interest;
+            interestsSection.appendChild(div);
+        });
     })
     .catch(error => {
-        console.error("Chyba při načítání:", error); 
+        console.error('Chyba:', error);
+        document.getElementById('name').textContent = "Chyba načítání profilu";
     });
